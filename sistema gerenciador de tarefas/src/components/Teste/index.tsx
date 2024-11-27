fbc451704a7ca636eed17c003a4db821dad0d5ee
rafa <rafaelcostatricolor@gmail.com>
2024-11-27 02:03:25 -0300

    projeto React criado

---
+++ sistema gerenciador de tarefas/src/components/Teste/index.tsx
@@ -0,0 +1,16 @@
+//código teste em .tsx para fins de demonstração.
+//aqui deve ser escrito código html com js e exportado para ser usado no  App.tsx
+
+import { P  } from "./styles" //import do TS que criamos
+
+export type props = {    //tipagem criada e exportada para poder alterar os elementos no CSS 
+children: string; //tipo do elemento
+tipo?: 'principal' | 'secundario'; //variavel criada que só assume dois valores
+fontSize?: number; //tamanho da fonte
+}
+
+//feito isso podemos alterar dinâmicamente agora o nosso elemento HTML
+
+const Paragrafo = ({children, tipo = 'principal', fontSize}: props) => <P fontSize={fontSize} tipo={tipo}>{children}</P> 
+
+export default Paragrafo
