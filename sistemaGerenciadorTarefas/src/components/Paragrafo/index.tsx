74f0c782be6d71d63322d2f8d5e9443b69d13979
rafa <rafaelcostatricolor@gmail.com>
2024-12-11 16:08:45 -0300

    adicionado a mudança entre forms na tela de login

--- sistemaGerenciadorTarefas/src/components/Paragrafo/index.tsx
+++ sistemaGerenciadorTarefas/src/components/Paragrafo/index.tsx
@@ -4,10 +4,11 @@ export type props = {
 children: string; 
 tipo?: 'cinza' | 'preto' | 'branco'; 
 fontSize?: number; 
+cursor?: "pointer" | "none"; 
 }
 
 
 
-const Paragrafo = ({children, tipo = 'cinza', fontSize = 14}: props) => <P fontSize={fontSize} tipo={tipo}>{children}</P> 
+const Paragrafo = ({children, tipo = 'cinza', cursor = 'none', fontSize = 14}: props) => <P fontSize={fontSize} tipo={tipo} cursor={cursor}>{children}</P> 
 
 export default Paragrafo
--- sistemaGerenciadorTarefas/src/components/Paragrafo/styles.ts
+++ sistemaGerenciadorTarefas/src/components/Paragrafo/styles.ts
@@ -7,4 +7,5 @@ export const P = styled.p<props>`
     font-size:${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')}; 
     color: ${(props) => (props.tipo === 'cinza' ? '#949494' : props.tipo === 'branco' ? '#ffffff' : '#282a35')};
     line-height: auto;
+    cursor: ${(props) => (props.cursor === 'pointer' ? 'pointer' : 'none')};
 `
